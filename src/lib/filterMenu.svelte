5fe9f83298a09f55e3e6b97861fe7f1da589180b
Ryank2004 <ryankorver04@gmail.com>
2024-11-15 14:04:33 +0100

    Icons toegevoegd

--- src/lib/filterMenu.svelte
+++ src/lib/filterMenu.svelte
@@ -16,16 +16,17 @@
 
   onMount(() => {
     document.addEventListener('click', handleClickOutside);
-    return () => document.removeEventListener('click', handleClickOutside);
+    // return () => document.removeEventListener('click', handleClickOutside);
   });
 </script>
 
 <button class="filter_button" on:click={() => (filterMenuVisible = !filterMenuVisible)} aria-expanded="{filterMenuVisible}">
-  FILTER
+  FILTER <svg class="filter_icon" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="white"><path d="M440-160q-17 0-28.5-11.5T400-200v-240L168-736q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42L560-440v240q0 17-11.5 28.5T520-160h-80Zm40-308 198-252H282l198 252Zm0 0Z"/></svg>
 </button>
 
 {#if filterMenuVisible}
-  <div class="filter_menu" transition:fly="{{ x: -300, duration: 300 }}">
+  <div class="filter_menu" transition:fly="{{ x: -270, duration: 300 }}">
+    <svg class="close_icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="var(--primary-color)"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
     <button class="category_button" on:click={() => selectCategory()}>BREAST</button>
     <button class="category_button" on:click={() => selectCategory()}>GASTROINTESTINAL</button>
     <button class="category_button" on:click={() => selectCategory()}>GYNAECOLOGY</button>
@@ -39,14 +40,27 @@
 
 <style>
   .filter_button {
+    display: inline-flex;
     background-color: var(--primary-color);
     color: white;
+    align-items: center;
     outline: none;
     border: none;
-    padding: 15px 30px;
+    padding: 4.75px 10px;
     border-radius: 10px;
     margin: 50px 0 0 50px;
     border: 3px solid var(--primary-color);
+  }
+
+  .filter_icon {
+    margin-left: 10px;
+    font-size: 20px;
+  }
+
+  .close_icon {
+    margin-left: 230px;
+    width: 30px;
+    height: 30px;
   }
 
   .filter_button:hover {
@@ -55,9 +69,11 @@
   }
 
   .filter_menu {
+    /* display: flex;
+    flex-direction: column; */
     position: fixed;
     top: 0;
-    width: 300px;
+    width: 270px;
     height: 100%;
     background-color: #f9f9f9;
     border-right: 1px solid #ccc;
@@ -81,4 +97,5 @@
     background-color: var(--primary-color);
     color: white;
   }
+
 </style>
